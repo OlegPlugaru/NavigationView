a5234806310e0abec8416cd215ad9ed53219ab05
Oleg Plugaru <oleg.plugaru.md@gmail.com>
2023-05-21 14:51:09 +0300

    Initial Commit

---
+++ NavigationView/NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewApp.swift
+//  NavigationView
+//
+//  Created by Whoami on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
